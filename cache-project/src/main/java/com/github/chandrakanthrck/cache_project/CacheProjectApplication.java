3be73f14dff365556ecc9ad4ec4d08804f6c797a
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-09-20 16:31:22 -0700

    Spring Initialzr setup

---
+++ cache-project/src/main/java/com/github/chandrakanthrck/cache_project/CacheProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.chandrakanthrck.cache_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheProjectApplication.class, args);
+	}
+
+}
---
+++ cache-project/src/test/java/com/github/chandrakanthrck/cache_project/CacheProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chandrakanthrck.cache_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
