96419e7614ae625919d77f54da2360e47b811c99
chandrakanthrck <chandrakanthr789@gmail.com>
2024-11-03 14:04:59 -0800

    update test

--- cache-project/src/test/java/com/github/chandrakanthrck/cache_project/CacheProjectApplicationTests.java
+++ cache-project/src/test/java/com/github/chandrakanthrck/cache_project/CacheProjectApplicationTests.java
@@ -3,11 +3,16 @@ package com.github.chandrakanthrck.cache_project;
 import com.github.chandrakanthrck.cache_project.service.SynchronizedCacheService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+//@ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class CacheProjectApplicationTests {
 
     @Autowired
