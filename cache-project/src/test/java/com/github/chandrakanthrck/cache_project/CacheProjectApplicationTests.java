1e5fffb9b8135cf4e5911e9955b42db12abe56f9
chandrakanthrck <chandrakanthr789@gmail.com>
2024-11-03 14:11:45 -0800

    update test

--- cache-project/src/test/java/com/github/chandrakanthrck/cache_project/CacheProjectApplicationTests.java
+++ cache-project/src/test/java/com/github/chandrakanthrck/cache_project/CacheProjectApplicationTests.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 //@ActiveProfiles("test")
 @SpringBootTest
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
 class CacheProjectApplicationTests {
 
     @Autowired
